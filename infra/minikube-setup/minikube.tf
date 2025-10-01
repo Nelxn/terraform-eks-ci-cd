a4aad6076bbf5a8d4b2e31207542834e26266f00
Nelxn <nelsonasoh812@gmail.com>
2025-10-01 21:39:01 +0100

    edit

--- infra/minikube-setup/minikube.tf
+++ infra/minikube-setup/minikube.tf
@@ -1,7 +1,7 @@
-resource "null_resource" "start_minikube" {
+/* resource "null_resource" "start_minikube" {
   provisioner "local-exec" {
     command = "minikube start --driver=docker --cpus=2 --memory=2g"
   }
-}
+/*}
 
 
