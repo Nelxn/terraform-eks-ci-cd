e1d9cb4493adecb742d3d5414615e1a32b71a758
Nelxn <nelsonasoh812@gmail.com>
2025-10-04 12:24:21 +0100

    edit

--- infra/ec2-for-jenkins/output.tf
+++
@@ -1,4 +0,0 @@
-# for output purpose
-output "ec2_public_ip" {
-  value = aws_instance.myapp-ec2.public_ip
-}
--- infra/ec2-for-jenkins/variables.tf
+++
@@ -1,7 +0,0 @@
-# these variables can be defined in a separate variables.tf file or in a terraform.tfvars file
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_name" {}
-variable "my_ip" {}
-variable "instance_type" {}
